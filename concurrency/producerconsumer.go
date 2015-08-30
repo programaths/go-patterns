83051e850bdee3f016cf309f010575313d474084
Jason Buberel <jason@buberel.org>
2015-08-30 09:54:18 -0700

    Fixing race condition

--- concurrency/producerconsumer.go
+++ concurrency/producerconsumer.go
@@ -1,11 +1,8 @@
-# Producer-Consumer
-
-```go
 package main
 
 
 import (
-  "fmt"
+	"fmt"
 )
 
 var done = make(chan bool)
@@ -14,10 +11,10 @@ var msgs = make(chan int)
 // produce sends integers to the msgs channel
 // and closes the channel upon completion.
 func produce() {
-  defer close(msgs)
-  for i := 0; i < 10; i++ {
-    msgs <- i
-  }
+	defer close(msgs)
+	for i := 0; i < 10; i++ {
+		msgs <- i
+	}
 }
 
 // consume will pull msgs off of the channel
@@ -26,10 +23,10 @@ func produce() {
 // the for statement will terminate, and consume
 // will signal on the done channel.
 func consume() {
-  for msg := range msgs {
-    fmt.Println(msg)
-  }
-  done <- true
+	for msg := range msgs {
+		fmt.Println(msg)
+	}
+	done <- true
 }
 
 // After creating the produce and consume 
@@ -37,8 +34,7 @@ func consume() {
 // wait until the done channel is signalled
 // (by the consume function)
 func main() {
-  go produce()
-  go consume()
-  <-done
+	go produce()
+	go consume()
+	<-done
 }
-```
